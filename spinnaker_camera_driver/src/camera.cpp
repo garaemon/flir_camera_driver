a1d30fd87f5b8b2c63fd18842cedf99d7669f6dd
GITAI <admin@gitai.tech>
2020-10-29 14:08:09 +0900

    Fix for trigger to work

--- spinnaker_camera_driver/src/camera.cpp
+++ spinnaker_camera_driver/src/camera.cpp
@@ -85,16 +85,21 @@ void Camera::setNewConfiguration(SpinnakerConfig& config, const uint32_t& level)
     // Set Trigger and Strobe Settings
     // NOTE: The trigger must be disabled (i.e. TriggerMode = "Off") in order to configure whether the source is
     // software or hardware.
-    setProperty(node_map_, "TriggerMode", std::string("Off"));
+    // setProperty(node_map_, "TriggerMode", std::string("Off"));
+    setProperty(node_map_, "TriggerMode", config.enable_trigger);
     setProperty(node_map_, "TriggerSource", config.trigger_source);
     setProperty(node_map_, "TriggerSelector", config.trigger_selector);
     setProperty(node_map_, "TriggerActivation", config.trigger_activation_mode);
-    setProperty(node_map_, "TriggerMode", config.enable_trigger);
 
+    if (config.v3_3_selector != "Off")
+    {
+      setProperty(node_map_, "LineSelector", config.v3_3_selector);
+      setProperty(node_map_, "V3_3Enable", true);
+    }
     setProperty(node_map_, "LineSelector", config.line_selector);
     setProperty(node_map_, "LineMode", config.line_mode);
     setProperty(node_map_, "LineSource", config.line_source);
-    setProperty(node_map_, "V3_3Enable", config.v3_3_enable);
+
 
     // Set auto exposure
     setProperty(node_map_, "ExposureMode", config.exposure_mode);
